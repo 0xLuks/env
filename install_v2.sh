d984cd8cad98cfa8c09920e46a5fdba82e56317b
Luks <51793738+luksecurity@users.noreply.github.com>
2024-05-23 17:23:34 +0200

    Update install_v2.sh

--- install_v2.sh
+++ install_v2.sh
@@ -23,7 +23,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt-get install -yq arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
 sudo apt-get install -yq libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
 sudo apt-get install -yq libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev
-sudo apt-get install -yq curl vim zsh python3-venv
+sudo apt-get install -yq curl vim zsh python3-venv bash-completion
 
 # Fonts
 ((current_step++))
@@ -75,7 +75,8 @@ if ! pipx list | grep -q 'exegol'; then
     step 75 "Installing exegol"
     pipx install exegol
     sudo usermod -aG docker $(id -u -n)
-    newgrp docker
+    echo "Please log out and log back in to apply the Docker group changes."
+    register-python-argcomplete --no-defaults exegol | sudo tee /etc/bash_completion.d/exegol > /dev/null
 else
     step 75 "Exegol is already installed, skipping."
 fi
