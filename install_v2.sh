9b4d67326cc5d6bbcb3f0d8356e454c2ca743963
Luks <51793738+luksecurity@users.noreply.github.com>
2024-05-23 17:02:11 +0200

    Update install_v2.sh

--- install_v2.sh
+++ install_v2.sh
@@ -23,7 +23,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt-get install -yq arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
 sudo apt-get install -yq libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
 sudo apt-get install -yq libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev
-sudo apt-get install -yq curl vim zsh
+sudo apt-get install -yq curl vim zsh python3-venv
 
 # Fonts
 ((current_step++))
